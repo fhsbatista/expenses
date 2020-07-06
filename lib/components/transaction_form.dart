71776fac5caeda9212cf04c35af3014c0e07b7f3
Fernando Batista <fernandohsbatista@gmail.com>
2020-07-06 19:00:48 -0300

    Fixed "add transaction" form making it responsive to the available size when the keyboard is visibile

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -43,56 +43,113 @@ class _TransactionFormState extends State<TransactionForm> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _titleController,
-              decoration: InputDecoration(
-                labelText: 'Título',
-              ),
-            ),
-            TextFormField(
-              controller: _valueController,
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Valor',
-              ),
-              onFieldSubmitted: (_) => _onSubmit(),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    _selectedDate == null
-                        ? 'Nenhuma data selecionada'
-                        : 'Data Selecionada: ${DateFormat('dd/MM/yyyy').format(_selectedDate)}',
-                  ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10.0,
+            right: 10.0,
+            left: 10.0,
+            bottom: 10.0 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: 'Título',
                 ),
-                FlatButton(
-                  child: Text(
-                    'Selecionar Data',
-                  ),
-                  textColor: Theme.of(context).primaryColor,
-                  onPressed: _openDatePicker,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('Nova Transação'),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Theme.of(context).textTheme.button.color,
-                  onPressed: _onSubmit,
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
                 ),
-              ],
-            ),
-          ],
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              TextFormField(
+                controller: _valueController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                ),
+                onFieldSubmitted: (_) => _onSubmit(),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'Nenhuma data selecionada'
+                          : 'Data Selecionada: ${DateFormat('dd/MM/yyyy').format(_selectedDate)}',
+                    ),
+                  ),
+                  FlatButton(
+                    child: Text(
+                      'Selecionar Data',
+                    ),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: _openDatePicker,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Nova Transação'),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).textTheme.button.color,
+                    onPressed: _onSubmit,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       elevation: 5,
